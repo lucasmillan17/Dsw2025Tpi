a658ff73b24eea47fe5fe817e251d2e14d1293a1
Lucas Millan <Lucas.Millan@alu.frt.utn.edu.ar>
2025-06-25 13:03:28 -0300

    DbContext 1st definition

--- Dsw2025Tpi.Data/Dsw2025TpiContext.cs
+++ Dsw2025Tpi.Data/Dsw2025TpiContext.cs
@@ -1,7 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dsw2025Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dsw2025Tpi.Data;
 
 public class Dsw2025TpiContext: DbContext
 {
+    public Dsw2025TpiContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Product>(eb => {
+            eb.ToTable("Products");
+            eb.Property(p => p.InternalCode).HasMaxLength(20).IsRequired();
+            eb.Property(p =>p.Name).HasMaxLength(50);
+            eb.Property(p =>p.Description).HasMaxLength(60);
+
+        });
+    }
 }
