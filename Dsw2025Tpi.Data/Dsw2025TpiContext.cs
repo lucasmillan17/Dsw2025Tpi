cf6d0c119e3446efab2c9eb31e06e47e8e51d607
Lucas Millan <Lucas.Millan@alu.frt.utn.edu.ar>
2025-06-25 13:22:13 -0300

    save changes db context before merge

--- Dsw2025Tpi.Data/Dsw2025TpiContext.cs
+++ Dsw2025Tpi.Data/Dsw2025TpiContext.cs
@@ -14,10 +14,17 @@ public class Dsw2025TpiContext: DbContext
         base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<Product>(eb => {
             eb.ToTable("Products");
-            eb.Property(p => p.InternalCode).HasMaxLength(20).IsRequired();
-            eb.Property(p =>p.Name).HasMaxLength(50);
-            eb.Property(p =>p.Description).HasMaxLength(60);
-
+            eb.Property(p => p.Sku).IsRequired();
+            eb.Property(p => p.InternalCode).HasMaxLength(20);
+            eb.Property(p => p.Name).HasMaxLength(50);
+            eb.Property(p => p.Description).HasMaxLength(60);
+            eb.Property(p => p.CurrentUnitPrice).HasPrecision(15, 2);
+        });
+        modelBuilder.Entity<Order>(eb => {
+            eb.Property(p => p.TotalAmount).HasPrecision(15, 2);
+            eb.Property(p => p.ShippingAddress).HasMaxLength(60);
+            eb.Property(p => p.BillingCity).HasMaxLength(60);
+            eb.Property(p => p.Notes).HasMaxLength(60);
         });
     }
 }
