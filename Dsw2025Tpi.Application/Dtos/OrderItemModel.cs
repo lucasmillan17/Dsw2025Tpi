936c2bf754571407fdfa73bb2bf02c18a5ee5d6d
Lucas Millan <Lucas.Millan@alu.frt.utn.edu.ar>
2025-06-25 13:20:19 -0300

    Corrected dtos

--- Dsw2025Tpi.Application/Dtos/OrderItemModel.cs
+++ Dsw2025Tpi.Application/Dtos/OrderItemModel.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Dsw2025Tpi.Application.Dtos
 {
-    internal class OrderItem
+    public record OrderItemModel
     {
+        public record Request(
+            [Required]string ProductCode,
+            [Required][Range(0,int.MaxValue)] int Quantity
+            );
+
+        public record Response(
+            string Name,
+            string Description,
+            decimal CurrentUnitPrice
+            );
     }
 }
