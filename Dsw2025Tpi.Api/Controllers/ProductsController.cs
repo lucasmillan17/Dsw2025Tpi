296639f0fb492c7a5373a76e46ab9fbeb2736f1f
Lucas Millan <Lucas.Millan@alu.frt.utn.edu.ar>
2025-06-27 16:38:21 -0300

    Fix case: controllers → Controllers

--- Dsw2025Tpi.Api/Controllers/ProductsController.cs
+++ Dsw2025Tpi.Api/Controllers/ProductsController.cs
@@ -1,6 +1,9 @@
-﻿using Dsw2025Tpi.Application.Services.Interfaces;
+using Dsw2025Tpi.Application.Dtos;
+using Dsw2025Tpi.Application.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-
+using Dsw2025Tpi.Application.Exceptions;
+using System;
+using System.Threading.Tasks;
 namespace Dsw2025Tpi.Api.Controllers
 {
     [ApiController]
@@ -8,9 +11,49 @@ namespace Dsw2025Tpi.Api.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly IProductService _productService;
-        public ProductsController(IProductService productService) { 
+        public ProductsController(IProductService productService)
+        {
             _productService = productService;
         }
-        
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] ProductModel.Request request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var product = await _productService.CreateProductAsync(request);
+                return CreatedAtAction(nameof(GetProductById), new { id = product.ProductId }, product);
+            }
+            catch (DuplicatedItemException ex) { 
+                return BadRequest(ex.Message);
+            }
+            
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(Guid id)
+        {
+            try {
+                var product = await _productService.GetProductByIdAsync(id);
+                return Ok(product);
+            }
+            catch (NotFoundException ex) {
+                return NotFound();
+            }
+            
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllProductsAsync()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            if (products == null || !products.Any())
+            {
+                return NoContent();
+            }
+            return Ok(products);
+        }
+       
     }
 }
